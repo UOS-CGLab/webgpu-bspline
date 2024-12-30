7eaa87434688f80c4adea073dc42ca2a89e4a6c0
inmare <inmare2000@proton.me>
2024-12-31 03:49:09 +0900

    불필요한 셰이더 삭제

--- src/ts/webgpu/computeBlend.ts
+++ src/ts/webgpu/computeBlend.ts
@@ -74,7 +74,6 @@ export default async function computeBlend(device: GPUDevice, circleUV: Float32A
     // 이전 식에서 bi와 bj를 따로 계산하는 형태의 셰이더
     @compute @workgroup_size(controlPointLen)
     fn main (
-      @builtin(global_invocation_id) global_invocation_id: vec3u,
       @builtin(workgroup_id) workgroup_id: vec3u,
       @builtin(local_invocation_id) local_invocation_id: vec3u,
     ) {
@@ -83,8 +82,7 @@ export default async function computeBlend(device: GPUDevice, circleUV: Float32A
 
       let circleX: f32 = calcBlend(i, circleUV[idx].x);
       let circleY: f32 = calcBlend(i, circleUV[idx].y);
-      // let temp: vec2f = vec2f(global_invocation_id.xy);
-      // blendResult[idx * controlPointLen + i] = temp; 
+
       blendResult[idx * controlPointLen + i] = vec2f(circleX, circleY);
     }
     `,
