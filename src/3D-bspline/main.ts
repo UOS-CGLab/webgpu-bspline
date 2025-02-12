d0b560b657d38c5e409932d35854340f5581e85e
inmare <yatsumehole@naver.com>
2025-02-13 01:28:07 +0900

    여러개를 그리는 코드 추가

--- src/3D-bspline/main.ts
+++ src/3D-bspline/main.ts
@@ -24,9 +24,13 @@ const module = device.createShaderModule({
 		matrix: mat4x4f,
 	};
 
+	struct Color {
+		color: vec4f,
+	}
+
 	struct Vertex {
 		@location(0) position: vec4f,
-		@location(1) color: vec4f,
+		// @location(1) color: vec4f,
 	};
 
 	struct VSOutput {
@@ -35,11 +39,12 @@ const module = device.createShaderModule({
 	};
 
 	@group(0) @binding(0) var<uniform> uni: Uniforms;
+	@group(0) @binding(1) var<uniform> color: Color;
 
 	@vertex fn vs(vert: Vertex) -> VSOutput {
 		var vsOut: VSOutput;
 		vsOut.position = uni.matrix * vert.position;
-		vsOut.color = vert.color;
+		vsOut.color = color.color;
 		return vsOut;
 	}
 
@@ -56,10 +61,10 @@ const pipeline = device.createRenderPipeline({
 		module,
 		buffers: [
 			{
-				arrayStride: 4 * 4, // 4 float * 4 byte
+				arrayStride: 3 * 4, // 4 float * 4 byte
 				attributes: [
 					{shaderLocation: 0, offset: 0, format: 'float32x3'},
-					{shaderLocation: 1, offset: 12, format: 'unorm8x4'},
+					// {shaderLocation: 1, offset: 12, format: 'unorm8x4'},
 				],
 			},
 		],
@@ -70,6 +75,7 @@ const pipeline = device.createRenderPipeline({
 	},
 	primitive: {
 		cullMode: 'front',
+		topology: 'line-list',
 	},
 	depthStencil: {
 		depthWriteEnabled: true,
@@ -86,21 +92,30 @@ const uniformBuffer = device.createBuffer({
 	usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
 });
 const uniformValues = new Float32Array(uniformBufferSize / 4);
-
 // Offsets to the various uniform values in float32 indices
 const kMatrixOffset = 0;
-
 const matrixValue = uniformValues.subarray(kMatrixOffset, kMatrixOffset + 16);
-
-const matrix = mat4.create(
-	2 / canvasElem.clientWidth, 0, 0, 0,
-	0, -2 / canvasElem.clientHeight, 0, 0,
-	0, 0, 0.5 / 400, 0,
-	-1, 1, 0.5, 1,
-);
+// const matrix_o = mat4.create(
+// 	2 / canvasElem.clientWidth, 0, 0, 0,
+// 	0, -2 / canvasElem.clientHeight, 0, 0,
+// 	0, 0, 0.5 / 400, 0,
+// 	-1, 1, 0.5, 1,
+// );
+const matrix = mat4.perspective(utils.degToRad(45), canvas.width / canvas.height, 1, 1000);
 matrixValue.set(matrix);
 
-// The color will not change so let's set it once at init time
+const colorBufferSize = (4) * 4;
+const colorBuffer = device.createBuffer({
+	label: 'color',
+	size: colorBufferSize,
+	// eslint-disable-next-line no-bitwise
+	usage: GPUBufferUsage.UNIFORM | GPUBufferUsage.COPY_DST,
+});
+const colorValues = new Float32Array(colorBufferSize / 4);
+const colorValue = colorValues.subarray(kMatrixOffset, kMatrixOffset + 4);
+const color = new Float32Array([1, 0, 0, 1]);
+colorValue.set(color);
+device.queue.writeBuffer(colorBuffer, 0, colorValues);
 
 const {vertexData, numVertices} = createFvertices();
 const vertexBuffer = device.createBuffer({
@@ -116,6 +131,7 @@ const bindGroup = device.createBindGroup({
 	layout: pipeline.getBindGroupLayout(0),
 	entries: [
 		{binding: 0, resource: {buffer: uniformBuffer}},
+		{binding: 1, resource: {buffer: colorBuffer}},
 	],
 });
 
@@ -173,14 +189,18 @@ function render(time?: number) {
 	pass.setPipeline(pipeline);
 	pass.setVertexBuffer(0, vertexBuffer);
 
-	const delta = time ?? 0;
-	const rad = utils.degToRad(delta / 20);
+	// const delta = time ?? 0;
+	// const rad = utils.degToRad(delta / 20);
+	const rad = utils.degToRad(0);
 
-	const t = mat4.translation([canvas.width / 2, canvas.height / 2, 0]);
+	const t = mat4.translation([-100, -100, -400]);
 	const r = mat4.rotationX(rad);
 	mat4.rotateY(r, rad, r);
 	mat4.rotateZ(r, rad, r);
-	const m = mat4.multiply(mat4.multiply(matrix, t), r);
+
+	const s = mat4.scaling([0.5, 0.5, 0.5]);
+
+	const m = mat4.multiply(matrix, mat4.multiply(mat4.multiply(t, r), s));
 	matrixValue.set(m);
 
 	device.queue.writeBuffer(uniformBuffer, 0, uniformValues);
@@ -202,13 +222,13 @@ function createFvertices() {
 		-50,
 		-50,
 		50,
-		 50,
-		-50,
 		50,
 		-50,
 		50,
+		-50,
 		50,
-		 50,
+		50,
+		50,
 		50,
 		50,
 
@@ -219,10 +239,10 @@ function createFvertices() {
 		-50,
 		50,
 		-50,
-		 50,
+		50,
 		-50,
 		-50,
-		 50,
+		50,
 		50,
 		-50,
 
@@ -233,10 +253,10 @@ function createFvertices() {
 		-50,
 		50,
 		50,
-		 50,
+		50,
 		50,
 		-50,
-		 50,
+		50,
 		50,
 		50,
 
@@ -244,27 +264,27 @@ function createFvertices() {
 		-50,
 		-50,
 		-50,
-		 50,
+		50,
 		-50,
 		-50,
 		-50,
 		-50,
 		50,
-		 50,
+		50,
 		-50,
 		50,
 
 		// 오른쪽 면
-		 50,
-		-50,
-		-50,
-		 50,
 		50,
 		-50,
-		 50,
 		-50,
 		50,
-		 50,
+		50,
+		-50,
+		50,
+		-50,
+		50,
+		50,
 		50,
 		50,
 
@@ -328,101 +348,38 @@ function createFvertices() {
 		23,
 	];
 
-	const quadColors = [
-		// 앞면: 빨강
-		255,
-		0,
-		0,
-		0,
-		255,
-		0,
-		0,
-		255,
-		0,
-		0,
-		0,
-		255,
-		// 뒷면: 초록
-		255,
-		255,
-		0,
-		255,
-		255,
-		0,
-		0,
-		255,
-		0,
-		0,
-		255,
-		0,
-		// 윗면: 파랑
-		0,
-		0,
-		255,
-		0,
-		0,
-		255,
-		0,
-		0,
-		255,
-		0,
-		0,
-		255,
-		// 밑면: 노랑
-		255,
-		255,
-		0,
-		255,
-		255,
-		0,
-		255,
-		255,
-		0,
-		255,
-		255,
-		0,
-		// 오른쪽 면: 자홍
-		255,
-		0,
-		255,
-		255,
-		0,
-		255,
-		255,
-		0,
-		255,
-		255,
-		0,
-		255,
-		// 왼쪽 면: 청록
-		0,
-		255,
-		255,
-		0,
-		255,
-		255,
-		0,
-		255,
-		255,
-		0,
-		255,
-		255,
-	];
+	const cubeLength = 9;
+	const cubeOffset = 50;
 
-	const numVertices = indices.length;
-	const vertexData = new Float32Array(numVertices * 4); // Xyz + color
-	const colorData = new Uint8Array(vertexData.buffer);
+	const numVertices = indices.length * (cubeLength ** 3);
+	const vertexData = new Float32Array(numVertices * 3);
 
-	for (const [i, index] of indices.entries()) {
-		const positionNdx = index * 3;
-		const position = positions.slice(positionNdx, positionNdx + 3);
-		vertexData.set(position, i * 4);
+	for (let cubeX = 0; cubeX < cubeLength; cubeX++) {
+		for (let cubeY = 0; cubeY < cubeLength; cubeY++) {
+			for (let cubeZ = 0; cubeZ < cubeLength; cubeZ++) {
+				for (const [i, index] of indices.entries()) {
+					const positionNdx = index * 3;
+					const position = positions.slice(positionNdx, positionNdx + 3);
+					position[0] += cubeX * cubeOffset;
+					position[1] += cubeY * cubeOffset;
+					position[2] -= cubeZ * cubeOffset;
 
-		const quadNdx = Math.trunc(i / 6) * 3;
-		const color = quadColors.slice(quadNdx, quadNdx + 3);
-		colorData.set(color, (i * 16) + 12); // Set RGB
-		colorData[(i * 16) + 15] = 255; // Set A
+					// console.log((i + (indices.length * (cubeZ + ((cubeLength ** 2) * (cubeY + (cubeX * cubeLength)))))));
+
+					// console.log(i + (index * (cubeX + (cubeY * cubeLength))));
+
+					vertexData.set(position, (
+						i + (
+							indices.length * (cubeZ + ((cubeLength * cubeLength) * (cubeY + (cubeX * cubeLength)))))) * 3);
+				}
+			}
+		}
 	}
+	// for (const [i, index] of indices.entries()) {
+	// 	const positionNdx = index * 3;
+	// 	const position = positions.slice(positionNdx, positionNdx + 3);
+	// 	vertexData.set(position, i * 3);
+	// }
 
 	return {
 		vertexData,
