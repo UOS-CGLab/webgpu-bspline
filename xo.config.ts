5de1c3da6ce700cebd145079b0dffbce503633db
inmare <inmare2000@proton.me>
2025-07-16 09:58:48 +0900

    설정 변경

--- xo.config.ts
+++ xo.config.ts
@@ -7,7 +7,7 @@ const xoConfig: FlatXoConfig = [
 			'unicorn/filename-case': 'off', // 파일 이름 camelcase 강제 방지
 			'@typescript-eslint/naming-convention': 'off', // React등 파일 이름에 camel case 이외의 이름 허용
 			'@typescript-eslint/no-restricted-types': 'off', // Jsx에 대한 undefined 대신 null입력 허용
-			'no-bitwise': 'off',
+			'no-bitwise': 'off', // 비트 연산자 사용 허용
 		},
 		prettier: true,
 	},
